66a5e316aee7147dd8a528e325ba97a6b8848160
Vincent <marignier.vincent@gmail.com>
2025-01-22 09:09:21 +0100

    ✨ Add hot reload to nest

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setBaseViewsDir(join(__dirname, '..', 'src','views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
   app.setViewEngine('ejs'); // Définir EJS comme moteur de rendu
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
