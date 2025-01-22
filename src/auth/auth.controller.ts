2a01f4a555a421db9ca67093ed78edcc541e665f
Vincent <marignier.vincent@gmail.com>
2025-01-22 10:43:32 +0100

    ✨ Add css, js files links, icons and fonts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
   @Get('france-connect')
   @Render('france-connect')
   franceConnect() {
-    const loginUrl = this.authService.getFranceConnectUrl('your-client-id', 'http://localhost:3000/auth/callback');
+    const loginUrl = this.authService.getFranceConnectUrl('7f16ffb1099c05049bb41b6a6453f3b2cb981358765a055328b339b3b0e053d8', 'https://fs.vmarigner.docker.dev-franceconnect.fr/api/login-callback');
     return {
       title: 'Connexion avec FranceConnect',
       loginUrl,
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('ejs'); // Définir EJS comme moteur de rendu
   await app.listen(process.env.PORT ?? 3000);
   if (module.hot) {
