c87049c6749b96562f99f0b99b0d0f7d85f31929
Vincent <marignier.vincent@gmail.com>
2025-01-29 12:58:03 +0100

    ✨ Add session management

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as fs from 'fs';
+import * as session from 'express-session';
 
 declare const module: any;
 
@@ -11,10 +12,21 @@ async function bootstrap() {
     key: fs.readFileSync('./secrets/key.pem'),
     cert: fs.readFileSync('./secrets/cert.pem'),
   };
-  
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions
+    httpsOptions,
   });
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: true,
+        maxAge: 3600000,
+      },
+    }),
+  );
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('ejs'); // Définir EJS comme moteur de rendu
