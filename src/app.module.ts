9aed0937a969ff8a3e364998f828d3a89c35458d
Vincent <marignier.vincent@gmail.com>
2025-01-29 12:58:31 +0100

    🎨 Use france connect service instead of auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FranceConnectModule } from './france-connect/france-connect.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { FranceConnectService } from './france-connect/france-connect.service';
 
 @Module({
-  imports: [FranceConnectModule, AuthModule, ConfigModule.forRoot()],
+  imports: [FranceConnectModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FranceConnectService],
 })
 export class AppModule {}
