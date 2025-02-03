4cc146c4a76c4f4004673891959b886435aea4ad
Vincent <marignier.vincent@gmail.com>
2025-02-03 11:07:38 +0100

    🎨 Use config service instead of url

--- src/france-connect/france-connect.module.ts
+++ src/france-connect/france-connect.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FranceConnectService } from './france-connect.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [FranceConnectService]
+  providers: [FranceConnectService, ConfigService],
 })
 export class FranceConnectModule {}
--- src/france-connect/france-connect.service.ts
+++ src/france-connect/france-connect.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class FranceConnectService {
+  constructor(private readonly configService: ConfigService) {}
   getFranceConnectUrl(clientId: string, redirectUri: string): string {
     const state = uuidv4();
     const nonce = uuidv4();
-    return `https://fcp-low.integ01.dev-franceconnect.fr/api/v2/authorize?response_type=code&acr_values=eidas1&scope=openid+gender+given_name+family_name+email+preferred_username&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&nonce=${nonce}`;
+    return `${this.configService.get<string>('FC_URL')}${this.configService.get<string>('AUTHORIZE_URL')}?response_type=code&acr_values=eidas1&scope=openid+gender+given_name+family_name+email+preferred_username&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&nonce=${nonce}`;
   }
 
   decodeJwtPayload(token: string): Record<string, any> | null {
